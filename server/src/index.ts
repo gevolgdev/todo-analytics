956d757d931c6f3f003c32f62183587fd4a6729e
Pinelli <ricardosilvapinelli@gmail.com>
2023-07-25 11:25:48 -0300

    index server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,14 @@
 import express from 'express';
 import authRoutes from './routes/authRoutes';
+import { usersDB } from './controllers/authController';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/auth', authRoutes);
-
-app.get('/', (req, res) => {
-  res.send('Oi')
-});
 
 app.listen(port, () => {
   console.log(`Server is running in ${port}!`);
