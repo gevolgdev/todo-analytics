881783feb2cbf211cd3c5ef10bf2d96982c24cf5
Pinelli <ricardosilvapinelli@gmail.com>
2023-07-25 11:21:24 -0300

    client: Rotas

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Home, Login, Register } from './routes/index.ts';
 import App from './App.tsx'
-import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+    children: [
+      {
+        path: '/',
+        element: <Home/>
+      },
+      {
+        path: '/login',
+        element: <Login/>
+      },
+      {
+        path: '/register',
+        element: <Register/>
+      },
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
