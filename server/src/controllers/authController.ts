3169ab0e5512ae8ce558c0f52e1541d39ff961d1
Pinelli <ricardosilvapinelli@gmail.com>
2023-07-25 11:18:44 -0300

    authController: Salvando em banco de dados

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,39 +1,56 @@
-import RequestProps from "../models/RequestProps";
+import { Request, Response } from "express";
 import UserProps from "../models/user";
 import { hashPassword, comparePasswords, generateToken } from "../services/authService";
+import db from '../database/db';
 
-const usersDB: UserProps[] = [];
+export const usersDB: UserProps[] = [];
 
-export const register = async ({ req, res }: RequestProps): Promise<void> => {
+export const register = async (req: Request, res: Response): Promise<void> => {
   const { name, email, password } = req.body;
   const hashedPassword = await hashPassword(password);
-  
-  const newUser: UserProps = {
-    id: Number(usersDB.length + 1),
-    name,
-    email,
-    password: hashedPassword,
-  };
-  usersDB.push(newUser);
 
-  const token = generateToken(newUser);
-  res.json({ user: newUser, token });
+  try {
+    const insertQuery = 'INSERT INTO `users` (`name`, `email`, `password`) VALUES (?, ?, ?)';
+    await db.query(insertQuery, [name, email, hashedPassword]);
+
+    const newUser: UserProps = {
+      name: name,
+      email: email,
+      password: hashedPassword,
+    };
+
+    const token = generateToken(newUser);
+    res.json({ user: newUser, token });
+    
+  } catch (error) {
+    console.error('Erro ao registrar o usuário:', error);
+    res.status(500).json({ message: 'Erro ao registrar o usuário.' });
+  }
 };
 
-export const login = async ({ req, res }: RequestProps): Promise<any> => {
+export const login = async (req: Request, res: Response): Promise<any> => {
   const { email, password } = req.body;
-  const user = usersDB.find(item => item.email === email);
 
-  if(!user) {
-    return res.status(404).json({ message: 'Usuário não encontrado' });
-  };
+  try {
+    const selectQuery = 'SELECT * from `users` WHERE `email` = ?';
+    const [rows] = await db.query(selectQuery, [email]);
 
-  const isPasswordValid = await comparePasswords(password, user.password);
+    if (!Array.isArray(rows) || rows.length === 0) {
+      return res.status(404).json({ message: 'Usuário não encontrado' });
+    }
 
-  if(!isPasswordValid) {
-    return res.status(401).json({ message: 'Credênciais inválidas.' });
-  };
+    const user: UserProps = rows[0] as UserProps;
 
-  const token = generateToken(user);
-  res.json({ user, token });
+    const isPasswordValid = await comparePasswords(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Credenciais inválidas.' });
+    }
+
+    const token = generateToken(user);
+    res.json({ user, token });
+  } catch (error) {
+    console.error('Erro ao realizar o login:', error);
+    res.status(500).json({ message: 'Erro ao realizar o login.' });
+  }
 };
