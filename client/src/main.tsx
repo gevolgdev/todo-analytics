984f41cc6501f51e0e95bbae51a94dc4ad838d1a
Pinelli <ricardosilvapinelli@gmail.com>
2023-07-27 23:27:43 -0300

    Main: Redux e autenticação de rota

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { Home, Login, Register } from './routes/index.ts';
+import { useSelector } from 'react-redux';
+import { Home, Login, Register } from './routes';
 import App from './App.tsx'
+// Redux
+import { Provider } from 'react-redux';
+import store from './lib/redux/store';
+import { RootState } from './lib/redux/reducer.ts';
+
+const AuthenticatedRoute = () => {
+  const isAuthenticated = useSelector(( state: RootState ) => state.userSlice.isAuthenticated);
+  return isAuthenticated ? <Home/> : <Register/>
+};
 
 const router = createBrowserRouter([
   {
@@ -11,7 +21,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element: <Home/>
+        element: <AuthenticatedRoute/>
       },
       {
         path: '/login',
@@ -27,6 +37,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <Provider store={store}>
+      <RouterProvider router={router}/>
+    </Provider>
   </React.StrictMode>,
 )
