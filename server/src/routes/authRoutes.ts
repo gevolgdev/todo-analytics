5f45cdec05124bab4a9429172bf52db9b27e88fe
Pinelli <ricardosilvapinelli@gmail.com>
2023-07-27 23:40:26 -0300

    authRoutes: Rota de tasks

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,9 +1,13 @@
 import express from "express";
 import { register, login } from "../controllers/authController";
+import { authMiddleware } from '../middlewares/authMiddleware';
+import { taskController } from "../controllers/tasksController";
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+
+router.get('/tasks', authMiddleware, taskController);
 
 export default router;
