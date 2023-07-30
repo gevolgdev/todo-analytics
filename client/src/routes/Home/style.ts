c6c91426eed1cda8d44aa0d7b1a999afc7ab2ecd
Pinelli <ricardosilvapinelli@gmail.com>
2023-07-30 14:38:44 -0300

    Home: style

--- client/src/routes/Home/style.ts
+++ client/src/routes/Home/style.ts
@@ -3,25 +3,28 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 4rem 3rem 2rem;
+  padding: 2rem;
 
   h1 {
-    font-size: 2rem;
-    color: white;
+    font-size: 1.5rem;
+    color: var(--detailText-color);
+    margin-bottom: 1rem;
   }
 
   .tasks {
     display: flex;
-    flex-direction: column;
+    flex-direction: column-reverse;
     gap: 20px;
+  }
+`;
 
-    .task {
-      font-family: 'Inter Tight', sans-serif;
-      font-size: 1rem;
-      color: white;
-      background: #323644;
-      padding: 1rem;
-      border-radius: 8px;
-    }
+export const Image = styled.div`
+  display: flex;
+  background: rgb(64, 123, 255, 0.3);
+  justify-content: center;
+  padding: 1rem 0;
+
+  img {
+    width: 200px;
   }
 `;
