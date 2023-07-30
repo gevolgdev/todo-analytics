5934797b49ded88dc9351146606ceab4ccf52eb3
Pinelli <ricardosilvapinelli@gmail.com>
2023-07-30 14:39:06 -0300

    GlobalStyle: variaveis e ajustes

--- client/src/style/GlobalStyle.ts
+++ client/src/style/GlobalStyle.ts
@@ -2,26 +2,36 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   :root {
-    font-family: 'DM Serif Display', serif;
+    --background-color: #272A37;
+    --secondary-color: #2089E7;
+    --gray-color: #323644;
+    --detailText-color: #878F9F;
+    --white-color: #edede9;
+    --red-color: #ef4444;
+
+    font-family: 'Inter Tight', sans-serif;
     font-size: 16px;
-    /* font-family: 'Inter Tight', sans-serif; */
     background-color: #272A37;
     font-synthesis: none;
     text-rendering: optimizeLegibility;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     -webkit-text-size-adjust: 100%;
-
-    @media (max-width: 800px) {
-      overflow: hidden;
-    }
   }
+
+  *{ padding: 0; margin: 0; box-sizing: border-box }
 
   body {
     margin: 0 auto;
-    max-width: 800px;
+    max-width: 400px;
     min-height: 100vh;
+    scrollbar-width: none; /* Firefox */
+    -ms-overflow-style: none; /* Internet Explorer 10+ */
   }
+
+  body::-webkit-scrollbar {
+  display: none; /* Chrome, Safari e outros navegadores WebKit */
+}
 
   button {
     font-family: 'Inter Tight', sans-serif;
