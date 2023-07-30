663153aa3333b91500fcbfb6bc95a06211614026
Pinelli <ricardosilvapinelli@gmail.com>
2023-07-30 15:16:40 -0300

    authRoutes: tasks separada

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,13 +1,9 @@
 import express from "express";
 import { register, login } from "../controllers/authController";
-import { authMiddleware } from '../middlewares/authMiddleware';
-import { taskController } from "../controllers/tasksController";
 
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
-
-router.get('/tasks', authMiddleware, taskController);
 
 export default router;
