820891d0c4bf3224206c1f5f8207f5de0b219164
Pinelli <ricardosilvapinelli@gmail.com>
2023-07-30 14:35:25 -0300

    Utils: addNewTask

---
+++ client/src/utils/Api/addNewTask.ts
@@ -0,0 +1,18 @@
+import api from '../../service';
+import { NewTaskProps } from '../../@types/types';
+
+const addNewTask = async ({ id, title, description, date, done }: NewTaskProps, token: string) => {
+  
+  const newTask: NewTaskProps = { id, title, description, date, done };
+  
+  try {
+    const response = await api.post('/tasks/new-task', newTask , {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+    console.log('addNewTask ', response.data);
+  } catch (err) {
+    console.log('Error no front-end ', err);
+  };
+};
+
+export default addNewTask;
