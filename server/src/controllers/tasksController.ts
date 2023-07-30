3f966e1ec236e7712a1cb5bdaac0cae51cf97dc3
Pinelli <ricardosilvapinelli@gmail.com>
2023-07-30 15:07:42 -0300

    taskController: Novos controladores

--- server/src/controllers/tasksController.ts
+++ server/src/controllers/tasksController.ts
@@ -1,16 +1,45 @@
 import { Request, Response } from "express";
 import db from "../database/db";
 
-export const taskController = async ( req: Request, res: Response ): Promise<any> => {
+export const getTasks = async ( req: Request, res: Response ): Promise<any> => {
   const user_id = req.query.user_id;
   try {
-    const insertQuery = 'SELECT * FROM `todo_lists` WHERE `user_id` = ?';
+    const insertQuery = 'SELECT * FROM `users_tasks` WHERE `user_id` = ?';
     const tasks = await db.query(insertQuery, [user_id]);
 
     console.log( tasks );
     res.json({ tasks });
 
   } catch (err) {
+    console.log('Erro no getTasks: ', err);
     res.json({ Error: err });
   };
 };
+
+export const addTask = async ( req: Request, res: Response ) => {
+  const { id, title, description, date, done } = req.body;
+
+  try {
+    const insertQuery = 'INSERT INTO `users-schema`.`users_tasks` (`user_id`, `title`, `description`, `date`, `done`) VALUES (?, ?, ?, ?, ?);';
+    db.query(insertQuery, [ id, title, description, date, done ]);
+    res.status(201).json({ message: 'Task added successfully' });
+
+  } catch (err) {
+    console.log('Erro no addNewTask: ', err);
+    res.status(500).json({ error: 'An error occurred while adding the task' });
+  };
+};
+
+// export const addTask = async ( req: Request, res: Response ) => {
+//   const { id, title, description, date, done } = req.body;
+
+//   try {
+//     const insertQuery = 'INSERT INTO `users-schema`.`users_tasks` (`user_id`, `title`, `description`, `date`, `done`) VALUES (?, ?, ?, ?, ?);';
+//     db.query(insertQuery, [ id, title, description, date, done ]);
+//     res.status(201).json({ message: 'Task added successfully' });
+
+//   } catch (err) {
+//     console.log('Erro no addNewTask: ', err);
+//     res.status(500).json({ error: 'An error occurred while adding the task' });
+//   };
+// };
