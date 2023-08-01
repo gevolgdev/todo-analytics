d55dfe5917eb2609d07e437a8b2a5f7da27dc184
Pinelli <ricardosilvapinelli@gmail.com>
2023-08-01 12:31:34 -0300

    tasksRouter: Rota para completar task

--- server/src/routes/tasksRoutes.ts
+++ server/src/routes/tasksRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/authMiddleware";
-import { getTasks, addTask } from "../controllers/tasksController";
+import { getTasks, addTask, completeTask } from "../controllers/tasksController";
 
 const router = express.Router();
 
 router.get('/get-tasks', authMiddleware, getTasks);
 router.post('/new-task', authMiddleware, addTask);
+router.post('/complete-task', authMiddleware, completeTask);
 
 export default router;
