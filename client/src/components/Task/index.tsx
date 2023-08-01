0ad702790a393bff5037b37466270079ffddd396
Pinelli <ricardosilvapinelli@gmail.com>
2023-08-01 12:30:13 -0300

    Task: função para completar tarefa

--- client/src/components/Task/index.tsx
+++ client/src/components/Task/index.tsx
@@ -2,23 +2,34 @@ import React from 'react';
 import { TaskProps } from '../../@types/types';
 import Container from './style';
 import { BsCalendarDateFill, BsCheck } from 'react-icons/bs';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../lib/redux/reducer';
+import { completeTaskReducer } from '../../lib/redux/slices/tasksSlice';
 
 
-const Task: React.FC<TaskProps> = ({ title, description, date, done, index }) => {
+const Task: React.FC<TaskProps> = ({ title, description, date, done }) => {
+
+  const Dispatch = useDispatch();
 
   let isDone: boolean = false;
   const setIsDone = () => {
     if(done == '0') return isDone = false;
     if(done == '1') return isDone = true;
   };
-
   setIsDone();
+
+  const { id, token } = useSelector(( state: RootState ) => state.userSlice);
+
+  const completedTask = () => {
+    const doneTask: string = String( done == '0' && '1' || done == '1' && '0');
+    Dispatch(completeTaskReducer({ doneTask, id , title, description, token} ));
+  };
 
   return (
     <Container isDone={ isDone }>
       <span className='date'><BsCalendarDateFill/> { date }</span>
       <header>
-        <button onClick={ () => console.log( index ) }>
+        <button onClick={ completedTask }>
           { isDone && <BsCheck/> }
         </button>
         <h2 className='title'>{ title }</h2>
