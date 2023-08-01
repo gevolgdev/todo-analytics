f9dc29ce32770e1347ee56c619d4fbcf5656c1e1
Pinelli <ricardosilvapinelli@gmail.com>
2023-08-01 12:26:20 -0300

    fetchUserTask: Função para puxar tasks do banco

---
+++ client/src/utils/Api/fetchUserTasks.ts
@@ -0,0 +1,24 @@
+import { Dispatch } from "redux";
+import api from "../../service";
+import { fetchTasks } from "../../lib/redux/slices/tasksSlice";
+
+interface FetchTasksProps {
+  id: number | undefined;
+  token: string;
+};
+
+const fetchUserTasks = async ({ id, token }: FetchTasksProps, dispatch: Dispatch) => {
+  try {
+    const response = await api.get('/tasks/get-tasks', {
+      headers: { Authorization: `Bearer ${token}` },
+      params: { user_id: id }
+    });
+
+    dispatch(fetchTasks(response.data.tasks[0]));
+    console.log('Fetch das tasks');
+  } catch (err) {
+    console.log('Erro no fetchUserTasks.ts: ', err);
+  }
+};
+
+export default fetchUserTasks;
