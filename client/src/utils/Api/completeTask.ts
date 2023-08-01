1825bfbfe4d7186eee9eec0344bb06d6fbdafb5f
Pinelli <ricardosilvapinelli@gmail.com>
2023-08-01 12:31:17 -0300

    completeTasks: Ajustes e correções

--- client/src/utils/Api/completeTask.ts
+++ client/src/utils/Api/completeTask.ts
@@ -1,21 +1,21 @@
-// import React from 'react'
 import api from '../../service';
 
 interface completeTaskProps {
-  id: number;
-  done: string;
+  title: string;
+  id: number | undefined;
+  doneTask: string;
+  description: string;
 };
 
-const completeTask = async ({ id, done}: completeTaskProps, token: string) => {
+const completeTask = async ( { doneTask, id: user_id, title, description }: completeTaskProps, token: string ) => {
   try {
-    const response = await api.post('/tasks/complete-task', { id, done }, {
+    const response = await api.post('/tasks/complete-task', { doneTask, user_id, title, description }, {
       headers: { Authorization: `Bearer ${token}`}
     });
-    console.log('Response completeTask - Utils: ', response.data);
-    
+    console.log('✓ Task feita: ', response.data);
   }
   catch (err) {
-    console.log('Error completeTask - Utils: ', err);
+    console.log('✕ Erro em completar task: ', err);
   }
 };
 
