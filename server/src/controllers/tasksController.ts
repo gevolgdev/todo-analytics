0cd84cccbf0cfb197e0b60b7e66268b67ed339fd
Pinelli <ricardosilvapinelli@gmail.com>
2023-08-01 12:29:32 -0300

    tasksController: Marcar como feito as task no banco

--- server/src/controllers/tasksController.ts
+++ server/src/controllers/tasksController.ts
@@ -4,7 +4,7 @@ import db from "../database/db";
 export const getTasks = async ( req: Request, res: Response ): Promise<any> => {
   const user_id = req.query.user_id;
   try {
-    const insertQuery = 'SELECT * FROM `users_tasks` WHERE `user_id` = ?';
+    const insertQuery = 'SELECT * FROM `users-schema`.`users_tasks` WHERE `user_id` = ?';
     const tasks = await db.query(insertQuery, [user_id]);
 
     console.log( tasks );
@@ -30,16 +30,16 @@ export const addTask = async ( req: Request, res: Response ) => {
   };
 };
 
-// export const addTask = async ( req: Request, res: Response ) => {
-//   const { id, title, description, date, done } = req.body;
+export const completeTask = async ( req: Request, res: Response ) => {
+  const { doneTask, user_id, title, description } = req.body;
 
-//   try {
-//     const insertQuery = 'INSERT INTO `users-schema`.`users_tasks` (`user_id`, `title`, `description`, `date`, `done`) VALUES (?, ?, ?, ?, ?);';
-//     db.query(insertQuery, [ id, title, description, date, done ]);
-//     res.status(201).json({ message: 'Task added successfully' });
+  try {
+    const insertQuery = 'UPDATE `users-schema`.`users_tasks` SET `done` = ? WHERE `user_id` = ? AND title = ? AND description = ?';
+    db.query(insertQuery, [ doneTask, user_id, title, description ]);
+    res.status(201).json({ message: 'Task completada!' });
 
-//   } catch (err) {
-//     console.log('Erro no addNewTask: ', err);
-//     res.status(500).json({ error: 'An error occurred while adding the task' });
-//   };
-// };
+  } catch (err) {
+    console.log('Erro no completeTask: ', err);
+    res.status(500).json({ error: 'An error occurred while completing the task' });
+  };
+};
