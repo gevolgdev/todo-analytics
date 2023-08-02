9328aca999a7b037e8b9a8aa96a9ca5a58763836
Pinelli <ricardosilvapinelli@gmail.com>
2023-08-02 18:30:27 -0300

    tasksRoutes: nova rota para deletar task

--- server/src/routes/tasksRoutes.ts
+++ server/src/routes/tasksRoutes.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/authMiddleware";
-import { getTasks, addTask, completeTask } from "../controllers/tasksController";
+import { getTasks, addTask, completeTask, editTask, deleteTask } from "../controllers/tasksController";
 
 const router = express.Router();
 
 router.get('/get-tasks', authMiddleware, getTasks);
 router.post('/new-task', authMiddleware, addTask);
 router.post('/complete-task', authMiddleware, completeTask);
+router.post('/edit-task', authMiddleware, editTask);
+router.post('/delete-task', authMiddleware, deleteTask);
 
 export default router;
