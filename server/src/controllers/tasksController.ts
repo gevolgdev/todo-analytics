7b3e35d418f5128453ab887e8f1f5dd61ff52b03
Pinelli <ricardosilvapinelli@gmail.com>
2023-08-02 18:30:10 -0300

    tasksController: requisiçoes para editar e excluir

--- server/src/controllers/tasksController.ts
+++ server/src/controllers/tasksController.ts
@@ -43,3 +43,29 @@ export const completeTask = async ( req: Request, res: Response ) => {
     res.status(500).json({ error: 'An error occurred while completing the task' });
   };
 };
+
+export const editTask = async ( req: Request, res: Response ) => {
+  const { title, description, user_id, task_id } = req.body;
+  try {
+    const insertQuery = 'UPDATE `users-schema`.`users_tasks` SET `title` = ?, `description` = ? WHERE `user_id` = ? AND `id` = ?';
+    db.query(insertQuery, [ title, description, user_id, task_id ]);
+    res.status(201).json({ message: 'Tarefa editada!' });
+  }
+  catch (err) {
+    console.log('Erro ao editar (Controller): ', err);
+    res.status(500).json({ error: 'An error occurred while editing the task' });
+  }
+};
+
+export const deleteTask = async ( req: Request, res: Response ) => {
+  const { task_id } = req.body;
+  try {
+    const insertQuery = 'DELETE FROM `users-schema`.`users_tasks` WHERE (`id` = ?);';
+    db.query(insertQuery, [ task_id ]);
+    res.status(201).json({ message: 'Tarefa excluida!' });
+  }
+  catch (err) {
+    console.log('Erro ao editar (Controller): ', err);
+    res.status(500).json({ error: 'An error occurred while editing the task' });
+  }
+};
