9aa91eea1160a503997596ec154aad0392ceeec6
Pinelli <ricardosilvapinelli@gmail.com>
2023-08-02 10:45:24 -0300

    completeTask: Api.post retirada da variavel

--- client/src/utils/Api/completeTask.ts
+++ client/src/utils/Api/completeTask.ts
@@ -9,10 +9,9 @@ interface completeTaskProps {
 
 const completeTask = async ( { doneTask, id: user_id, title, description }: completeTaskProps, token: string ) => {
   try {
-    const response = await api.post('/tasks/complete-task', { doneTask, user_id, title, description }, {
+    await api.post('/tasks/complete-task', { doneTask, user_id, title, description }, {
       headers: { Authorization: `Bearer ${token}`}
     });
-    console.log('✓ Task feita: ', response.data);
   }
   catch (err) {
     console.log('✕ Erro em completar task: ', err);
