1c04b9bb2672b386df762e46cd535f0710ac443b
Pinelli <ricardosilvapinelli@gmail.com>
2023-08-09 20:41:14 -0300

    Server: Nova rota "/users"

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import authRoutes from './routes/authRoutes';
 import tasksRoutes from './routes/tasksRoutes';
+import usersRoutes from './routes/usersRoutes';
 import cors from 'cors';
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/tasks', tasksRoutes);
+app.use('/users', usersRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running in ${port}!`);
