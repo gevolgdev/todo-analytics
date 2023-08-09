bb82f621aacdc5eeee2012e900b17441f1a3dd36
Pinelli <ricardosilvapinelli@gmail.com>
2023-08-09 20:36:35 -0300

    SocialSlice criado

--- client/src/lib/redux/reducer.ts
+++ client/src/lib/redux/reducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import userSlice from './slices/userSlice';
 import tasksSlice from './slices/tasksSlice';
+import socialSlice from './slices/socialSlice';
 
 export const rootReducer = combineReducers({
   userSlice,
   tasksSlice,
+  socialSlice,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
---
+++ client/src/lib/redux/slices/socialSlice.ts
@@ -0,0 +1,27 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
+import { UserProps } from '../../../@types/types';
+
+const initialState: UserProps[] = [
+  {
+    id: 0,
+    email: '',
+    name: '',
+    password: '',
+    token: '',
+    isAuthenticated: false
+  }
+];
+
+const socialSlice = createSlice({
+  name: 'social',
+  initialState,
+  reducers: {
+    fetchUsers: (state, { payload }: PayloadAction<UserProps[]>) => {
+      state = payload;
+      return state;
+    },
+  }
+});
+
+export const { fetchUsers } = socialSlice.actions;
+export default socialSlice.reducer;
