fcad599a25e2b45d6f39f6e57f70071f0917910a
Pinelli <ricardosilvapinelli@gmail.com>
2023-08-11 22:33:15 -0300

    userRoutes:adicionar e coletar amigos

--- server/src/routes/usersRoutes.ts
+++ server/src/routes/usersRoutes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { authMiddleware } from "../middlewares/authMiddleware";
-import { getUsers } from "../controllers/users";
+import { fetchFriends, getUsers, newFriend } from "../controllers/users";
 
 const router = express.Router();
 
 router.get('/get-users', authMiddleware, getUsers);
+router.get('/get-friends', authMiddleware, fetchFriends);
+router.post('/new-friend', authMiddleware, newFriend);
 export default router;
